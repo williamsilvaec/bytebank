c0e42157b13d75d86781b01febf4ee1896da2d00
william.silva <william@viasoft.com.br>
2018-02-12 14:44:13 -0200

    Funcionario

---
+++ src/TesteFuncionario.java
@@ -0,0 +1,14 @@
+public class TesteFuncionario {
+
+    public static void main(String args[]) {
+
+        Funcionario nico = new Funcionario();
+        nico.setNome("Nico Steppat");
+        nico.setCpf("124234154");
+        nico.setSalario(2600.0);
+
+        System.out.println("Nome: "+ nico.getNome());
+        System.out.println("Bonificação: " + nico.getBonificacao());
+
+    }
+}
